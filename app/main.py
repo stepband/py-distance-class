7dc15f2b1953ba478dc6325c8367d4e068ef21ba
stepband <vladshvik@gmail.com>
2023-06-15 22:50:37 +0300

    Partly implemented all magick methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,35 @@
+from typing import Union
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance is {self.km} kilometers"
+
+    def __repr__(self) -> str:
+        return f"km={self.km}"
+
+    def __add__(self, other: Union["Distance", int, float]) -> "Distance":
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        elif isinstance(other, (int, float)):
+            return Distance(self.km + other)
+        else:
+            raise 'Error'
+
+    def __add__(self, other: Union["Distance", int, float]) -> "Distance":
+        if isinstance(other, Distance):
+            self.km += other.km
+        elif isinstance(other, (int, float)):
+            self.km += other
+        else:
+            raise 'Error with +='
+        return self
+
+    def __mul__(self, other: Union[int, float]):
+        if isinstance(other, (int, float)):
+            self.km *= other
+
+
+
