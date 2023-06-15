1f357d28ac57ee996ced2dce5d3eb04f30a53e49
stepband <vladshvik@gmail.com>
2023-06-15 22:58:58 +0300

    Partly implemented all magick methods

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+
 class Distance:
     def __init__(self, km: int) -> None:
         self.km = km
@@ -18,7 +19,7 @@ class Distance:
         else:
             raise 'Error'
 
-    def __add__(self, other: Union["Distance", int, float]) -> "Distance":
+    def __iadd__(self, other: Union["Distance", int, float]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         elif isinstance(other, (int, float)):
@@ -27,9 +28,17 @@ class Distance:
             raise 'Error with +='
         return self
 
-    def __mul__(self, other: Union[int, float]):
+    def __mul__(self, other: Union[int, float]) -> "Distance":
         if isinstance(other, (int, float)):
-            self.km *= other
+            return Distance(self.km * other)
+        else:
+            raise "Error with *="
+
+    def __truediv__(self, other) -> "Distance":
+        if isinstance(other, (int, float)):
+            self.km /= other
+        else:
+            raise "Error with /="
 
 
 
